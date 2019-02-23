cd6655c67c3f7ecaa5537586ea97b55d961e67b6
dblkickd2 <47839045+dblkickd2@users.noreply.github.com>
2019-02-23 08:10:34 -0800

    Add files via upload

---
+++ PositiveQuotes.py
@@ -0,0 +1,13 @@
+from random import *
+
+print("Positive Quotes")
+print("--------------------")
+
+quotes = [ "You are awesome!" ,
+         "What excellent talent you possess." ,
+        "You communicate very well."
+         ]
+
+#print a random item in the 'quotes' list
+print(choice(quotes))
+print("You are awesome!")
